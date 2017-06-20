a35df4e614392234d1e990f1fc3e6e3ae12a4778
Rheiter <pim_rheiter@live.nl>
2017-06-20 18:18:23 +0100

    basedao

--- src/main/java/hu/ipass/persistence/BaseDAO.java
+++ src/main/java/hu/ipass/persistence/BaseDAO.java
@@ -13,7 +13,7 @@ public class BaseDAO {
 			
 			String username = "pisvtaeuldtswo";
 			String password = "a023eb13107af1cddd79af5e0319493ce0707a32fe51bb827d6de1a94a013ea6";
-		    String dbUrl = "a023eb13107af1cddd79af5e0319493ce0707a32fe51bb827d6de1a94a013ea6";
+		    String dbUrl = "jdbc:postgresql://ec2-79-125-13-42.eu-west-1.compute.amazonaws.com:5432/d6d9ll396lr3kd";
 
 		    con = DriverManager.getConnection(dbUrl, username, password);
 		} catch (ClassNotFoundException | SQLException e) {
